b22ac84753fe7e0c177eb56dc363c2abbc8b01a4
jessiyang1996 <jessiyang1996@gmail.com>
2024-01-17 15:15:22 -0600

    .map added, items display on DOM

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const todoRouter = require('./routes/todo.router.js');
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(express.json()); // needed for axios requests
+app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 // require('')
 import Form from '../Form/Form.jsx';
 import axios from 'axios'; // import axios in order to use it!
@@ -6,42 +6,26 @@ import axios from 'axios'; // import axios in order to use it!
 // a function represents a component!
 function App () {
   const [toDoList, setToDoList] = useState([
-    {
-      title: 'Go to Office Hours',
-      description: 'Office Hours are at 5PM-6PM'},
-    {
-      title: 'Go to work',
-      description: '7:45AM - 4:30PM',
-    }
-  ]);
+    {title: "Go to sleep early!", description: "Go to sleep at 9PM"},
+    {title: "Finish to-do App!", description: "Due next week!"}
 
-  // GET ROUTE
-  const getAppList = () => {
-    axios.get('/api/todo')
-      .then((response) => {
-        console.log(response.data);
-        setToDoList(response.data);
-      })
-      .catch((error) =>{
-        console.log("error in GET /api/todo", error);
-        alert('Something went wrong!');
-      })
-  };
+  ]);
 
   return (
     <div>
       <h1>TO DO APP</h1>
       <Form />
-      <ul>
-        {
-          toDoList.map((toDo) => {
-            return <li key={toDo.title}>{toDo.title}</li>
-          })
-        }
-      </ul>
+ 
+      {/* HOW TO RENDER A LIST */}
+      {toDoList.map((toDoItem, itemIndex) => {
+        return (
+          <ul key={itemIndex}>
+            <li kid="toDoItemTitle">{toDoItem.title}</li>
+            <p id="description">{toDoItem.description}</p>
+          </ul>
+        )
+      })};
     </div>
-  );
-
-}
+)} //end of App function
 
 export default App
