cd21ac591ece72cba33e5cfc71b173a6ec0a9822
jessiyang1996 <jessiyang1996@gmail.com>
2024-02-06 20:46:36 -0600

    Fixed error in pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,18 +1,19 @@
 const pg = require('pg');
 let pool;
 
-if (process.env.DATABASE_URL){ // new pool to connect to database that is rendered
+if (process.env.DATABASE_URL) {
+  // new pool to connect to database that is rendered
   pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
-    }
+    },
   });
 } else {
   pool = new pg.Pool({
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_DATABASE 'weekend-to-do-app', 
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || 'weekend-to-do-app',
   });
 }
 
